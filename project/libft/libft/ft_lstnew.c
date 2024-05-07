19eb3876da01b4e9d2b0cce0e475109f4ebafeb8
Shahanj Chowdhury <sschowdhury29@gmail.com>
2024-05-07 10:00:26 +0300

    ft_lstnew.c update

--- project/libft/libft/ft_lstnew.c
+++ project/libft/libft/ft_lstnew.c
@@ -6,11 +6,17 @@
 /*   By: shachowd <shachowd@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 09:04:14 by shachowd          #+#    #+#             */
-/*   Updated: 2024/05/07 09:06:49 by shachowd         ###   ########.fr       */
+/*   Updated: 2024/05/07 09:38:05 by shachowd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+typedef struct	s_list
+{
+	void			*content;
+	struct s_list	*next;
+};					t_list;
 
 t_list *ft_lstnew(void *content)
 {
