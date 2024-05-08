fad1276259be78f8e150e7d9133bfa4f2de6c001
Shahanj Chowdhury <sschowdhury29@gmail.com>
2024-05-08 11:14:42 +0300

    update: ft_lstadd_back.c -function added

--- project/libft/libft/ft_lstadd_back.c
+++ project/libft/libft/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: shachowd <shachowd@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 09:38:48 by shachowd          #+#    #+#             */
-/*   Updated: 2024/05/08 10:01:41 by shachowd         ###   ########.fr       */
+/*   Updated: 2024/05/08 11:12:09 by shachowd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,30 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	if (!lst)
+	if (!*lst)
 	{
 		*lst = new;
 	}
-	while (lst != NULL)
+	else
 	{
-		if ((*lst)->next == NULL)
-		{
-			(*lst)->next = new;
-			new->next = NULL;
-		}
+		ft_lstlast(*lst)->next = new;
 	}
 }
 /*
     - Adds the node ’new’ at the end of the list.
     - Lst:  The address of a pointer to the first link of a list.
     - 'new' The address of a pointer to the node to be added to the list.
+*/
+/* 
+    begin = NULL;
+    ft_lstadd_back(&begin, elem);
+    ft_lstadd_back(&begin, elem2);
+    ft_lstadd_back(&begin, elem3);
+    ft_lstadd_back(&begin, elem4);
+    while (begin)
+    {
+        ft_print_result(begin);
+        begin = begin->next;
+    }
+
 */
