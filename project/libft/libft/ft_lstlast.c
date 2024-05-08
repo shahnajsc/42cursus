d9149b39edc6f83fabeeeb9209108481d7db2d7d
Shahanj Chowdhury <sschowdhury29@gmail.com>
2024-05-08 09:35:16 +0300

    update: ft_lstlast -function added

--- project/libft/libft/ft_lstlast.c
+++ project/libft/libft/ft_lstlast.c
@@ -6,17 +6,25 @@
 /*   By: shachowd <shachowd@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 15:20:22 by shachowd          #+#    #+#             */
-/*   Updated: 2024/05/07 15:34:37 by shachowd         ###   ########.fr       */
+/*   Updated: 2024/05/08 09:35:16 by shachowd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstlast(t_list *lst)
+t_list	*ft_lstlast(t_list *lst)
 {
-    
+    if (lst == NULL)
+        return (NULL);
+	while (lst != NULL)
+	{
+        if (lst->next == NULL)
+            return (lst);
+        else
+		    lst = lst->next;
+	}
+	return (lst);
 }
-
 /*
 - Returns the last node of the list 'lst'.
 - 'lst' The beginning of the list
