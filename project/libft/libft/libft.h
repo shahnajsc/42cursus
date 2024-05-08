35780e633a70df511bac1c92b1abf68f906ecdb7
Shahanj Chowdhury <sschowdhury29@gmail.com>
2024-05-08 15:11:13 +0300

    update: ft_lstmap.c -function added

--- project/libft/libft/libft.h
+++ project/libft/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: shachowd <shachowd@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:02:57 by shachowd          #+#    #+#             */
-/*   Updated: 2024/05/08 14:30:08 by shachowd         ###   ########.fr       */
+/*   Updated: 2024/05/08 15:00:14 by shachowd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,5 +70,7 @@ void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void*));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
+						void (*del)(void *));
 
 #endif
