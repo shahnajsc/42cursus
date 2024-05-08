b4073c9b407c8e1659d04cadb87540ed618b9e80
Shahanj Chowdhury <sschowdhury29@gmail.com>
2024-05-08 10:07:53 +0300

    update: makefile libft -ft_lstadd_back

--- project/libft/libft/libft.h
+++ project/libft/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: shachowd <shachowd@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:02:57 by shachowd          #+#    #+#             */
-/*   Updated: 2024/05/08 09:12:49 by shachowd         ###   ########.fr       */
+/*   Updated: 2024/05/08 10:06:44 by shachowd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,5 +66,6 @@ t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
+void				ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
