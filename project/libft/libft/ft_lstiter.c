166c1cd02f06e9e480a3b18dbef13d6deb9782ae
Shahanj Chowdhury <sschowdhury29@gmail.com>
2024-05-08 14:38:01 +0300

    update: ft_lstiter.c -norm error fixed

--- project/libft/libft/ft_lstiter.c
+++ project/libft/libft/ft_lstiter.c
@@ -6,22 +6,23 @@
 /*   By: shachowd <shachowd@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 14:22:05 by shachowd          #+#    #+#             */
-/*   Updated: 2024/05/08 14:32:16 by shachowd         ###   ########.fr       */
+/*   Updated: 2024/05/08 14:37:02 by shachowd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_lstiter(t_list *lst, void (*f)(void *))
+void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-    while (lst)
+	while (lst)
 	{
 		f(lst->content);
 		lst = lst->next;
 	}
 }
 /*
-    - Iterates the list ’lst’ and applies the function ’f’ on the content of each node.
+    - Iterates the list ’lst’ and applies the function ’f’ on
+   		the content of each node.
     - 'lst' The address of a pointer to a node.
     - 'f' The address of the function used to iterate on the list.
 */
