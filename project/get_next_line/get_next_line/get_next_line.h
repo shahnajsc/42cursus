d71e0e7d681733f90cc29beb13803212d68d3fac
Shahanj Chowdhury <sschowdhury29@gmail.com>
2024-06-06 13:45:59 +0300

    update: header -function declarations

--- project/get_next_line/get_next_line/get_next_line.h
+++ project/get_next_line/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: shachowd <shachowd@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 10:31:55 by shachowd          #+#    #+#             */
-/*   Updated: 2024/06/05 12:00:11 by shachowd         ###   ########.fr       */
+/*   Updated: 2024/06/06 13:44:34 by shachowd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,11 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-
+size_t	    ft_strlen(const char *str);
+char        *ft_strdup(const char *str);
+char        *ft_strchr(const char *s, int c);
+char	    *ft_strjoin(const char *s1, const char *s2);
+char	    *ft_substr(const char *str, unsigned int start, size_t len);
 
 
 #endif
