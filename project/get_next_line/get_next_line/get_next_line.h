abf4385612dc3750554f79d656a6d364a90ce888
Shahanj Chowdhury <sschowdhury29@gmail.com>
2024-06-13 15:28:47 +0300

    update:

--- project/get_next_line/get_next_line/get_next_line.h
+++ project/get_next_line/get_next_line/get_next_line.h
@@ -3,33 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shahnaj <shahnaj@student.42.fr>            +#+  +:+       +#+        */
+/*   By: shachowd <shachowd@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 10:31:55 by shachowd          #+#    #+#             */
-/*   Updated: 2024/06/12 16:42:12 by shahnaj          ###   ########.fr       */
+/*   Updated: 2024/06/05 10:35:33 by shachowd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# ifndef BUFFER_SIZE
-
-#  define BUFFER_SIZE 6
-
-# endif
-
-# include <stdlib.h>
-# include <unistd.h>
-# include <fcntl.h>
-# include <limits.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 
-size_t	ft_strlen(char *str);
-char	*ft_strdup(char *str);
-char	*ft_strchr(char *s, int c);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_substr(char *str, unsigned int start, size_t len);
-char	*get_next_line(int fd);
+
 
 #endif
