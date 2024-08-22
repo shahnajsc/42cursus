59b61beb782804ad38a28cb880e35cd6e0a0a116
Shahnaj Sharmin Chowdhury <sschowdhury29@.com>
2024-08-22 22:45:21 +0300

    update: pipex function

--- project/pipex/pipex.c
+++ project/pipex/pipex.c
@@ -6,36 +6,65 @@
 /*   By: shachowd <shachowd@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 11:52:54 by shachowd          #+#    #+#             */
-/*   Updated: 2024/08/21 11:53:59 by shachowd         ###   ########.fr       */
+/*   Updated: 2024/08/22 17:53:00 by shachowd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include <error.h>
+#include <sys/wait.h>
+
+int	pid_wait(pid_t pid)
+{
+	int	status;
+
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+	return (5);
+}
 
 int pipex()
 {
-	
+	int i;
+	int status;
+	pid_t pid[2];
+
+	i = -1;
+	while ((++i) < 2)
+	{
+		pid[i] = fork();
+		if (pid[i] < 0)
+		{
+			printf("error in forking");
+		}
+		if(pid[i] != 0)
+		{
+			printf("parent id: %d  i :  %d\n", pid[i], i);
+		}
+	}
+	close(pid[i]);
+	i = -1;
+	while ((++i) < 2)
+		status = pid_wait(pid[i]);
+	return (status);
 }
 
 
 int main(int argc, char **argv, char **envp)
 {
-	char *msg = "test fd stdout\n";
-	int r;
+	int status;
 	
 	if (argc != 5)
 	{
-		printf("Wrong command format\nCorrect format: \"infile cmd1 cmd2 outfile\"\n)");
+		printf("Wrong command format\nCorrect format: \"infile cmd1 cmd2 outfile\"\n");
 		return (1);
 	}
 	else
 	{
-		r = write(0, msg, strlen(msg));
-		printf("%d\n", r);
-		perror("test ");
+		status = pipex();
+		printf("\nstatus print: %d\n", status);
 	}
     return 0;
 	
